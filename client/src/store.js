997ee0dcbb9236342755a72c19c5a4d0f3845f0f
zep1994 <snaxbags@protonmail.com>
2019-02-17 17:13:37 -0600

    Add Redux Store

---
+++ client/src/store.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import meetings from './reducers/meetings_reducer'
+
+
+const reducers = combineReducers({ meetings })
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default createStore(
+    reducers,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+)
