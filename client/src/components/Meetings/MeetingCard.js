0c653bed2c1d44736f36fdc9a39a697e5495b744
zep1994 <snaxbags@protonmail.com>
2019-02-23 19:41:03 -0600

    Add Price and Date

--- client/src/components/Meetings/MeetingCard.js
+++ client/src/components/Meetings/MeetingCard.js
@@ -3,7 +3,8 @@ import React from 'react'
 const MeetingCard = ({meeting}) => (
     <div key={meeting.id} className="MeetingCard">
         <h3>{meeting.name}</h3>
-        <h4>{meeting.date}</h4>
+        <p>{meeting.price}</p>
+        <p>{meeting.time}</p>
     </div>
      
 )
