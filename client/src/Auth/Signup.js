5a432d6bc45ba5c9ccd54276c6a648c074c53178
zep1994 <snaxbags@protonmail.com>
2019-03-19 17:32:08 -0500

    Setup Redux thunk

--- client/src/Auth/Signup.js
+++ client/src/Auth/Signup.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react'
+import { reduxForm, Field } from 'redux-form'
 
 class Signup extends Component {
+
+    onSubmit = formProps => {
+        console.log(formProps)
+    }
+
     render() {
+        const { handleSubmit } = this.props
         return (
-        <div>
-            <form>
-                <fieldset>
-                    <label>Email</label>
-                </fieldset>
-                <fieldset>
-                    <label>Password</label>
-                </fieldset>
-            </form>
-        </div>
+            <div>
+                <form onSubmit={handleSubmit(this.onSubmit)}>
+                    <fieldset>
+                        <label>Email</label>
+                        <Field
+                            name="email"
+                            component="input"
+                            type="text"
+                            placeholder="E-mail"
+                        />
+                    </fieldset>
+                    <fieldset>
+                        <label>Password</label>
+                        <Field
+                            name="password"
+                            component="input"
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </fieldset>
+                    <button>Sign Up</button>
+                </form>
+            </div>
         )
-        
+
     }
 }
 
-export default Signup
+export default reduxForm({ form: 'signup' })(Signup)
--- client/src/store.js
+++ client/src/store.js
@@ -2,9 +2,13 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import meetings from './reducers/meetings_reducer'
 import auth from './reducers/auth';
+import { reducer as formReducer } from 'redux-form'
 
-
-const reducers = combineReducers({ meetings, auth })
+const reducers = combineReducers({ 
+  meetings, 
+  auth,
+  form: formReducer
+})
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
